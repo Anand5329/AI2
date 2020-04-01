efd35ef560411f5ad607d287da261d9fd3e43f58
Anand5329 <anand.doshi.8@gmail.com>
2020-04-01 22:23:40 +0530

    Randomizing weights and biases in constructor. Implemented easier creation of Layers.

--- venv/Include/Layer.py
+++ venv/Include/Layer.py
@@ -1,11 +1,15 @@
 import Neurons
+import gc
 
 class Layer:
-    def __init__(self, neurons = []):
-        self.neurons = neurons
+    def __init__(self, previous_layer_size = 1, size = 1):
+        self.size = size
         self.biases = []
         self.values = []
-
-        for i in range(len(neurons)):
-            self.biases.append(neurons[i].bias)
-            self.values.append(neurons[i].value)
+        self.neurons = []
+        for i in range(self.size):
+            self.neurons.append(Neurons.Neuron(previous_layer_size = previous_layer_size))
+            self.neurons[i].randomize_weights()
+            self.neurons[i].randomize_bias()
+            self.biases.append(self.neurons[i].bias)
+            self.values.append(self.neurons[i].value)
