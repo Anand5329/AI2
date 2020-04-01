90c48bc283efb98cf9e0f021f5ac210c2fc253a6
Anand5329 <anand.doshi.8@gmail.com>
2020-04-01 22:21:03 +0530

    Testing layer.

--- venv/Include/Main.py
+++ venv/Include/Main.py
@@ -1,6 +1,8 @@
 import Neurons
-import NeuralNetworks
+import NeuralNetworks as NN
 import numpy as np
+import Layer as L
 
-a = Neurons.InputNeuron(bias = 1)
-print(a)
+# ann = NN.NeuralNetwork(inp = 5, hidden = [2, 2], output = 3)
+layer = L.Layer(1,5)
+pass
