123d9f46c6b13b311e65d33984155e3b2951377b
Anand5329 <anand.doshi.8@gmail.com>
2020-04-01 22:28:05 +0530

    TODO: fix bug in Layer.py

--- venv/Include/Layer.py
+++ venv/Include/Layer.py
@@ -8,6 +8,7 @@ class Layer:
         self.values = []
         self.neurons = []
         for i in range(self.size):
+            # TODO fix bug about weights not resetting. Acting like static field.
             self.neurons.append(Neurons.Neuron(previous_layer_size = previous_layer_size))
             self.neurons[i].randomize_weights()
             self.neurons[i].randomize_bias()
