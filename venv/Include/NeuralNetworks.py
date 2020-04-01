cf49672270131f65e6acc844fc7752345307b7f0
Anand5329 <anand.doshi.8@gmail.com>
2020-04-01 09:24:30 +0530

    Implemented Layer functionality. Made use of randomizers for bias and weights in create. Added forward prop method

--- venv/Include/NeuralNetworks.py
+++ venv/Include/NeuralNetworks.py
@@ -1,31 +1,50 @@
 import Neurons
+import Layer
 
 class NeuralNetwork:
-    def __init__(self, inp = 1, hidden = [1], output = 1, input_neurons = [], hidden_neurons = [[]], output_neurons = []):
+    def __init__(self, inp = 1, hidden = [1], output = 1, input_layer = Layer(), hidden_layers = [Layer()], output_layer = Layer()):
         self.input = inp
         self.hidden = hidden
         self.output = output
-        self.input_neurons = input_neurons
-        self.hidden_neurons = hidden_neurons
-        self.output_neurons = output_neurons
+        self.input_layer = input_layer
+        self.hidden_layers = hidden_layers
+        self.output_layer = output_layer
         self.create(self)
 
     def create(self):
+        neurons = []
         for i in range(self.inp):
-            self.input_neurons.append(Neurons.InputNeuron())
-            self.input_neurons[i].next_layer_size = self.hidden[0]
+            neurons.append(Neurons.InputNeuron())
+        self.input_layer = Layer(neurons)
 
         for i in range(self.hidden):
+            neurons = []
             for j in range(self.hidden[i]):
-
-                self.hidden_neurons[i].append(Neurons.HiddenNeuron())
-                if i+1 < len(hidden):
-                    self.hidden_neurons[i][j].next_layer_size = self.hidden[i+1]
+                neurons.append(Neurons.HiddenNeuron())
+                if i == 0:
+                    neurons[j].previous_layer_size = self.input
                 else:
-                    self.hidden_neurons[i][j].next_layer_size = self.output
+                    neurons[j].previous_layer_size = self.hidden[i-1]
+                neurons[j].randomize_weights()
+                neurons[j].randomize_bias()
+            self.hidden_layers.append(Layer(neurons))
 
+        neurons = []
         for i in range(self.ouptut):
             self.output_neurons.append(Neurons.OutputNeuron())
-            self.output_neurons[i].next_layer_size = 0
+            self.output_neurons[i].previous_layer_size = self.hidden[len(hidden)-1]
+            self.output_neurons[i].randomize_weights()
+            self.output_neurons[i].randomize_bias()
+        self.output_layer = Layer(neurons)
+
         return
+
+    def calculate_values(self):#forward propogation
+        for i in range(self.hidden):
+            for j in range(self.hidden[i]):
+                layer
+                if i == 0:
+                    layer = self.input_layer
+                else:
+                    layer = self.hidden_layers[i-1]
 
