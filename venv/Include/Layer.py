947e7cb0e9d1465ba261b884d4dc359814f26a97
Anand5329 <anand.doshi.8@gmail.com>
2020-04-02 09:11:49 +0530

    Added functionality to assign pre-existing values to layer.

--- venv/Include/Layer.py
+++ venv/Include/Layer.py
@@ -2,15 +2,18 @@ import Neurons
 import gc
 
 class Layer:
-    def __init__(self, previous_layer_size = 1, size = 1):
+    def __init__(self, previous_layer_size = 1, size = 1, values = []):
         self.size = size
         self.biases = []
         self.values = []
         self.neurons = []
+        if len(values) != size and len(values) != 0:
+            print("Input size mismatch")
         for i in range(self.size):
-            # TODO fix bug about weights not resetting. Acting like static field.
             self.neurons.append(Neurons.Neuron(previous_layer_size = previous_layer_size))
             self.neurons[i].randomize_weights()
             self.neurons[i].randomize_bias()
+            if len(values) == size:
+                self.neurons[i].value = values[i]
             self.biases.append(self.neurons[i].bias)
             self.values.append(self.neurons[i].value)
