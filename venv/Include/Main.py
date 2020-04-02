cc4ee8dd133814b6d89863f31478525d204aa4f6
Anand5329 <anand.doshi.8@gmail.com>
2020-04-02 09:12:48 +0530

    Testing forward prop with inputs. Working.

--- venv/Include/Main.py
+++ venv/Include/Main.py
@@ -3,6 +3,6 @@ import NeuralNetworks as NN
 import numpy as np
 import Layer as L
 
-# ann = NN.NeuralNetwork(inp = 5, hidden = [2, 2], output = 3)
-layer = L.Layer(1,5)
+ann = NN.NeuralNetwork(inp = 5, hidden = [2, 2], output = 3, inputs = [1, 2, 3, 4, 5])
+ann.forward_propagation()
 pass
