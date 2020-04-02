2e273deb945cbea62cd7d43ff79a5340018bba46
Anand5329 <anand.doshi.8@gmail.com>
2020-04-02 11:33:21 +0530

    Removed redundant derived classes for input, hidden and output neurons.

--- venv/Include/Neurons.py
+++ venv/Include/Neurons.py
@@ -20,15 +20,3 @@ class Neuron:
     def randomize_bias(self):
         self.bias = random.uniform(-1,1)
         return
-
-class InputNeuron(Neuron):
-    def __init__(self, bias=0, value=0, previous_layer_size = 0):
-        super().__init__(bias, value, previous_layer_size)
-
-class OutputNeuron(Neuron):
-    def __init__(self, bias=0, value=0, previous_layer_size = 0):
-        super().__init__(bias, value, previous_layer_size)
-
-class HiddenNeuron(Neuron):
-    def __init__(self, bias=0, value=0, previous_layer_size = 0):
-        super().__init__(bias, value, previous_layer_size)
