2645788e9651894a8360d9e95bf4b99374729d00
Anand5329 <anand.doshi.8@gmail.com>
2020-04-04 11:56:41 +0530

    Added update_values and iter_gen.

--- venv/Include/Layer.py
+++ venv/Include/Layer.py
@@ -2,6 +2,7 @@ import Neurons
 import gc
 
 class Layer:
+
     def __init__(self, previous_layer_size = 1, size = 1, values = []):
         self.size = size
         self.biases = []
@@ -17,3 +18,12 @@ class Layer:
                 self.neurons[i].value = values[i]
             self.biases.append(self.neurons[i].bias)
             self.values.append(self.neurons[i].value)
+
+    def iter_gen(self):
+        for i in self.neurons:
+            yield i
+
+    def update_values(self):
+        for n, v in zip(self.neurons, self.values):
+            n.value = v
+        return
