6f2a96ce95942f552acf35878a0f26305c42ddf1
Anand5329 <anand.doshi.8@gmail.com>
2020-04-04 11:59:27 +0530

    Added cost function: squared_mean_error.

---
+++ venv/Include/Cost.py
@@ -0,0 +1,10 @@
+import NeuralNetworks
+import Layer
+import math
+
+
+def squared_mean_error(needed_output, existing_output):
+    sum = 0
+    for x, y in zip(needed_output.values, existing_output):
+        sum = sum + abs(x ** 2 - y ** 2)
+    return sum / needed_output.size
