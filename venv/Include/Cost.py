dd644241852b13d984e98800d8bf050471d29340
Anand5329 <anand.doshi.8@gmail.com>
2020-04-05 08:07:29 +0530

    Changed name of cost function.

--- venv/Include/Cost.py
+++ venv/Include/Cost.py
@@ -3,7 +3,7 @@ import Layer
 import math
 
 
-def squared_mean_error(needed_output, existing_output):
+def mean_squared_error(needed_output, existing_output):
     sum = 0
     for x, y in zip(needed_output.values, existing_output):
         sum = sum + (x - y)**2
