84fca6d03296bfab180738b13dcf1a672befd398
Anand5329 <anand.doshi.8@gmail.com>
2020-04-23 11:25:49 +0530

    Better system of inputs and output. Still needs work. Implemented SGD.

--- src/NeuralArchitecture/Main.py
+++ src/NeuralArchitecture/Main.py
@@ -4,11 +4,19 @@ import src.NeuralArchitecture.Layer as L
 import numpy as np
 import random
 
-inputs = []
-for i in range(784):
-    inputs.append(random.uniform(0, 1))
-ann = NN.NeuralNetwork(inp = 784, hidden = [16, 16], output = 10, inputs = inputs)
-ann.forward_propagation()
+inputs = [[]]
+for j in range(0,100):
+    inputs.append([])
+    for i in range(784):
+        inputs[j].append(random.uniform(0, 1))
+ann = NN.NeuralNetwork(inp = 784, hidden = [16, 16], output = 10)
 needed_output = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
-print(NN.NeuralNetwork.mean_squared_error(needed_output, ann.output_layer.values))
+outputs = []
+for j in range(0,100):
+    outputs.append(needed_output)
+training_data = [inputs]
+training_data.append(outputs)
+ann.input_training_data(training_data)
+ann.stochastic_GD(10)
+print(ann.output_layer.values)
 pass
